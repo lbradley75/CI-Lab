39c1c5551fb1aefff8761326f388cdd20cfa7ccf
ip999 <porteous@gmail.com>
2019-03-06 22:25:50 +0000

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-const sum, product = require('./app');
+const { sum, product } = require('./app');
 
 test('adds 1 + 2 to equal 3', () => {
         expect(sum(1, 2)).toBe(3);
@@ -10,4 +10,8 @@ test('adds 100 + 100 to equal 200', () => {
 
 test('multiply 1 * 10 to equal 10', () => {
         expect(product(1, 10)).toBe(10);
+});
+
+test('multiply 3 * 200 to equal 600', () => {
+        expect(product(3, 200)).toBe(600);
 });

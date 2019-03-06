8e9c5a73487cb6dbadc777387625c85685c7341d
ip999 <porteous@gmail.com>
2019-03-06 22:20:42 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+//simple app
+
+//return the sum of 2 numbers
 function sum(a, b) {
   return a + b;
 }
 
-module.exports = sum;
-// comment
+
+//return the product of 2 numbers
+function product(a, b) {
+  return a * b;
+}
+
+//export functions
+module.exports = {
+    sum,
+    product
+}

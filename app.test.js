299166accc6e730c611a9249d89bb91e17878ded
ip999 <porteous@gmail.com>
2019-03-06 22:35:58 +0000

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,11 +1,23 @@
 const { sum, product } = require('./app');
 
+// sum function
+
+test('sum function exists', () => {
+  expect(sum).toBeDefined();
+});
+
 test('adds 1 + 2 to equal 3', () => {
         expect(sum(1, 2)).toBe(3);
 });
 
 test('adds 100 + 100 to equal 200', () => {
         expect(sum(100, 100)).toBe(200);
+});
+
+// product function
+
+test('product function exists', () => {
+  expect(product).toBeDefined();
 });
 
 test('multiply 2 * 2 to equal 4', () => {

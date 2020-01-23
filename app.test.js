bdc6978ebfca6acdbe65be7c637aabbcffe65cef
ip999 <porteous@gmail.com>
2020-01-23 14:00:34 +0000

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -10,8 +10,8 @@ test('adds 1 + 2 to equal 3', () => {
         expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 100 + 100 to equal 200', () => {
-        expect(sum(100, 100)).toBe(200);
+test('adds 101 + 101 to equal 200', () => {
+        expect(sum(101, 101)).toBe(202);
 });
 
 // product function
